9380d91d2bbd3031405ec28cbc39eb4df5e81175
Jojo <98564062+VanishScriptGod@users.noreply.github.com>
2022-11-11 17:43:44 -0500

    Update $Vanish.lua
    Added a few more games and scripts

--- $Vanish.lua
+++ $Vanish.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("KOed#7242 Made this", "BloodTheme")
+local Window = Library.CreateLib("Vanish by KOed#7242", "BloodTheme")
 --LightTheme
 --DarkTheme
 --GrapeTheme
