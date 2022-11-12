41b2a6aa51b1e0b2174a9ea6851a96930a7058ec
Jojo <98564062+VanishScriptGod@users.noreply.github.com>
2022-11-11 20:40:56 -0500

    Add files via upload

---
+++ %vanish part 2.lua
@@ -0,0 +1,42 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("KOed#7242 Made this", "Ocean")
+
+local Main = Window:NewTab("Work at a pizza place")
+local WorkpizzaSection = Main:NewSection("WAAPP")
+WorkpizzaSection:NewButton("Turk Gui", "Working 2022", function()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/TurkOyuncu99/bb2f1182a224999682a2d1a87aa8517a/raw/66c7791224be0434b23938ca26244238649d0143/ameicaa", true))()
+end)
+
+local Main = Window:NewTab("Slap battles")
+local SlapSection = Main:NewSection("Slap battles")
+
+
+SlapSection:NewButton("Slappy Gui", "Not what its called but whatever", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rblxscriptsnet/rblxhub/main/rblxhub.lua"))()
+end)
+
+
+
+
+
+local Main = Window:NewTab ("Counter Blox")
+local Countersection = Main:NewSection("Counter Blox")
+
+Countersection:NewButton("Hex gui", "Works well", function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Pawel12d/hexagon/main/scripts/CounterBlox.lua', true))()
+end)
+
+local Main = Window:NewTab ("Combat Warriors")
+local Combatsection = Main:NewSection("Combat Warriors")
+
+Combatsection:NewButton("Project hook gui", "Works well", function()
+loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()
+end)
+
+
+local Main = Window:NewTab ("Funky Friday")
+local Funksection = Main:NewSection("Funy Friday")
+
+Funksection:NewButton("Funky Player", "Works well", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/main.lua",true))()
+end)
